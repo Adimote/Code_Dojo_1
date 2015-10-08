a0dc79f58b48b63d3f3ef5c6c6f94290c8e00fe1
Andrew Barrett-Sprot <abs1g14@soton.ac.uk>
2015-10-08 20:12:30 +0100

    added features

--- helloworld.py
+++ helloworld.py
@@ -1,7 +1,31 @@
-def main():
-    print("Hello World!")
+import random
+from enum import Enum
+class Noun_Type(Enum):
+    unspecified = 0
+    animal = 1
+    human = 2
+    inanimate = 3
+
+class Article_Type(Enum):
+    improper = 0
+    human = 2
+    inanimate = 3
 
 
-# Run this on startup
-if __name__ == "__main__":
-    main()
+# Word before the noun
+
+nouns = [("cat", Noun_Type.animal), ("mat", Noun_Type.inanimate), ("banana", Noun_Type.inanimate), ("code dojo", Noun_Type.inanimate), ("Tom", Noun_Type.human)]
+
+verbs = ["cat", "mat", "banana", "code dojo", "Tom"]
+articles = [("The", Article_Type.improper), ("Their", Article_Type.improper), ("My", Article_Type.improper), ("A", Article_Type.improper), ("", Article_Type.human), ("Sir", Article_Type.human)]
+
+def pick_article_from_noun(noun_type):
+    if noun_type is not Noun_Type.human:
+        return random.choice([x for x in articles if x[1] is Article_Type.improper])
+    else:
+        return random.choice([x for x in articles if x[1] is Article_Type.human])
+
+
+noun = random.choice(nouns)
+article = pick_article_from_noun(noun[1])
+print("{0} {1}!".format(article[0], noun[0]))
