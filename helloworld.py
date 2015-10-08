bc59d330fb44760887e9aaac914b546d9274e7ed
Andrew Barrett-Sprot <abs1g14@soton.ac.uk>
2015-10-08 20:21:47 +0100

    added features

--- helloworld.py
+++ helloworld.py
@@ -1,10 +1,13 @@
 import random
 from enum import Enum
+
+
 class Noun_Type(Enum):
     unspecified = 0
     animal = 1
     human = 2
     inanimate = 3
+
 
 class Article_Type(Enum):
     improper = 0
@@ -12,12 +15,28 @@ class Article_Type(Enum):
     inanimate = 3
 
 
+class Verb_Type(Enum):
+    inanimate = 0
+    animate = 1
+
+
+class Prepositions(Enum):
+    _on = 0
+    _in = 1
+
+
 # Word before the noun
 
-nouns = [("cat", Noun_Type.animal), ("mat", Noun_Type.inanimate), ("banana", Noun_Type.inanimate), ("code dojo", Noun_Type.inanimate), ("Tom", Noun_Type.human)]
+nouns = [("cat", Noun_Type.animal), ("mat", Noun_Type.inanimate), ("banana", Noun_Type.inanimate),
+         ("code dojo", Noun_Type.inanimate), ("Tom", Noun_Type.human)]
 
-verbs = ["cat", "mat", "banana", "code dojo", "Tom"]
-articles = [("The", Article_Type.improper), ("Their", Article_Type.improper), ("My", Article_Type.improper), ("A", Article_Type.improper), ("", Article_Type.human), ("Sir", Article_Type.human)]
+articles = [("The", Article_Type.improper), ("Their", Article_Type.improper), ("My", Article_Type.improper),
+            ("A", Article_Type.improper), ("", Article_Type.human), ("Sir", Article_Type.human)]
+
+verbs = [("sat", Verb_Type.inanimate, [Prepositions._on, Prepositions._in]),
+         ("jumped", Verb_Type.animate, [Prepositions._on, Prepositions._in]),
+         ("ran", Verb_Type.inanimate, [Prepositions._on, Prepositions._in])]
+
 
 def pick_article_from_noun(noun_type):
     if noun_type is not Noun_Type.human:
